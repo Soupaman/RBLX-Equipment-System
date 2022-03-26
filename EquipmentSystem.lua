dcfa7af4e3d2bb5ba35a6e702c2530e025b64a05
Supa <36982153+Soupaman@users.noreply.github.com>
2022-03-25 23:01:41 -0700

    Create EquipmentSystem.lua

---
+++ EquipmentSystem.lua
@@ -0,0 +1,51 @@
+--[[
+
+Made by Supadownload for group 825742
+Do not edit. Do not copy. Do not distribute.
+
+--]]
+
+
+local EquipRFunction = game:GetService("ReplicatedStorage"):WaitForChild("EquipRFunction")
+local Equips = require(script.Equips)
+local Players = game:GetService("Players")
+
+local playerTable = {}
+
+local function AddToTable(plr)
+	table.insert(playerTable, plr)
+end
+
+local function RemoveFromTable(plr)
+	local plrIndex = table.find(playerTable, plr)
+	if plrIndex then
+		table.remove(playerTable, plrIndex)
+	end
+end
+
+local function EquipFunction(player, info)
+	local character = player.Character
+	if character then
+		local humanoid = character:FindFirstChild("Humanoid")
+		if humanoid then
+			if table.find(playerTable, player) then
+				return false
+			else
+				humanoid.Died:Connect(function()
+					RemoveFromTable(player)
+				end)
+				AddToTable(player)
+				humanoid.MaxHealth = 100 + (Equips.GetHealth(info))
+				print(player, "used an equip item: ",info)
+				return true
+			end
+		end
+	end
+	return false
+end
+
+Players.PlayerRemoving:Connect(function(player)
+	RemoveFromTable(player)
+end)
+
+EquipRFunction.OnServerInvoke = EquipFunction
