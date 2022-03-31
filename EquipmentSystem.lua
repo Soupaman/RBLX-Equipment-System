b21da5ad4bf4780aecc87ed0e3b3b1d1e634cbf0
Supa <36982153+Soupaman@users.noreply.github.com>
2022-03-31 01:28:55 -0700

    Update EquipmentSystem.lua
    Edited code so that equipments will be seen globally instead of locally now. Also added error code returns.

--- EquipmentSystem.lua
+++ EquipmentSystem.lua
@@ -7,8 +7,12 @@ Do not edit. Do not copy. Do not distribute.
 
 
 local EquipRFunction = game:GetService("ReplicatedStorage"):WaitForChild("EquipRFunction")
-local Equips = require(script.Equips)
+local EquipModelsStorage = game:GetService("ServerStorage").SupaWSystemSStorage.Equipables
 local Players = game:GetService("Players")
+local Equips = require(script.Equips)
+
+local WeldCopy = Instance.new("WeldConstraint")
+local ModelCopy = Instance.new("Model")
 
 local playerTable = {}
 
@@ -24,24 +28,51 @@ local function RemoveFromTable(plr)
 end
 
 local function EquipFunction(player, info)
+	if not info then
+		warn(player.Name .. " tried using an equipment item without permission.")
+		return 3
+	end
 	local character = player.Character
 	if character then
 		local humanoid = character:FindFirstChild("Humanoid")
 		if humanoid then
 			if table.find(playerTable, player) then
-				return false
+				return 2
 			else
 				player.CharacterRemoving:Connect(function()
 					RemoveFromTable(player)
 				end)
 				AddToTable(player)
-				humanoid.MaxHealth = humanoid.MaxHealth + (Equips.GetHealth(info))
-				print(player, "used an equip item: ",info)
-				return true
+				humanoid.MaxHealth = 100 + (Equips.GetHealth(info.Name))
+				local EquipModel = EquipModelsStorage[info.Name]
+				print(EquipModel)
+				if EquipModel then
+					EquipModel = EquipModel:Clone()
+
+					local Weld = Instance.new("WeldConstraint")
+					local ModelHolder = Instance.new("Model")
+					ModelHolder.Parent = character
+					
+					EquipModel.Handle.CFrame = character["Left Arm"].CFrame * CFrame.Angles(0,math.rad(90),0) * CFrame.new(0,.15,-0.59)
+					Weld.Part0 = character["Left Arm"]
+					Weld.Part1 = EquipModel.Handle
+					Weld.Parent = Weld.Part0
+					for _,v in pairs(EquipModel:GetChildren()) do
+						if v:IsA("BasePart") then
+							v.Parent = ModelHolder
+						end
+					end
+					EquipModel:Destroy()
+					
+					ModelHolder.Parent = character
+				end
+				
+				print(player, "used an equip item: ",info.Name)
+				return 1
 			end
 		end
 	end
-	return false
+	return 2
 end
 
 Players.PlayerRemoving:Connect(function(player)
